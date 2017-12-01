dc2f39968840a819cedb58677494aae1898df2ba
Huihuo Zheng <zhenghh04@gmail.com>
2017-11-30 22:38:51 -0600

    added arxiv aps format

--- Downfolding_review/combine_manuscript.sh
+++ Downfolding_review/combine_manuscript.sh
@@ -5,7 +5,7 @@ sed -e '/\input{intro.tex}/ {' -e 'r intro.tex' -e 'd' -e '}' \
     -e '/\input{theory.tex}/ {' -e 'r theory.tex' -e 'd' -e '}' \
     -e '/\input{threeband.tex}/ {' -e 'r threeband.tex' -e 'd' -e '}' \
     -e '/\input{hchain.tex}/ {' -e 'r hchain.tex' -e 'd' -e '}' \
-    -e '/\input{graphene.tex}/ {' -e 'r graphene.tex' -e 'd' -e '}' \
+    -e '/\input{honeycomb.tex}/ {' -e 'r honeycomb.tex' -e 'd' -e '}' \
     -e '/\input{fese.tex}/ {' -e 'r fese.tex' -e 'd' -e '}' \
     -e '/\input{conclusion.tex}/ {' -e 'r conclusion.tex' -e 'd' -e '}' \
     -e '/\input{appendix.tex}/ {' -e 'r appendix.tex' -e 'd' -e '}' \
